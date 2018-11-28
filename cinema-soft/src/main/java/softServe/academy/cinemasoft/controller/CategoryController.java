538a91897448eb7e4436fac4d8b58b4fedc1a1ea
Halmi Hodzhov <hhodztc@training.local>
2018-11-28 19:40:20 +0200

    change RestController to Controller

--- cinema-soft/src/main/java/softServe/academy/cinemasoft/controller/CategoryController.java
+++ cinema-soft/src/main/java/softServe/academy/cinemasoft/controller/CategoryController.java
@@ -1,10 +1,10 @@
 package softServe.academy.cinemasoft.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import softServe.academy.cinemasoft.service.CategoryService;
 
-@RestController
+@Controller
 public class CategoryController {
 
     @Autowired
--- cinema-soft/src/main/java/softServe/academy/cinemasoft/model/Category.java
+++ cinema-soft/src/main/java/softServe/academy/cinemasoft/model/Category.java
@@ -2,12 +2,15 @@ package softServe.academy.cinemasoft.model;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nameOfCategory;
 
