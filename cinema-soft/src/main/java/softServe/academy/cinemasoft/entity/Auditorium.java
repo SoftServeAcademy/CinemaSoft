60ef5b6785662a28328d8822ce31fa150f51c850
StoyanHriskov <stoyanhriskov@gmail.com>
2018-11-29 15:36:09 +0200

    added auditorium numberOfRows and seatsInRow

--- cinema-soft/src/main/java/softServe/academy/cinemasoft/entity/Auditorium.java
+++ cinema-soft/src/main/java/softServe/academy/cinemasoft/entity/Auditorium.java
@@ -10,36 +10,57 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class Auditorium {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	
+	private int numberOfRows;
+	private int seatsInRow;
+
 	@OneToMany
 	private List<Seat> seats;
-	
-	
+
 	public Integer getId() {
 		return this.id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return this.name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public List<Seat> getSeats() {
 		return this.seats;
 	}
+
 	public void setSeats(List<Seat> seats) {
 		this.seats = seats;
 	}
-	
-	
+
+	public int getNumberOfRows() {
+		return this.numberOfRows;
+	}
+
+	public void setNumberOfRows(int numberOfRows) {
+		this.numberOfRows = numberOfRows;
+	}
+
+	public int getSeatsInRow() {
+		return this.seatsInRow;
+	}
+
+	public void setSeatsInRow(int seatsInRow) {
+		this.seatsInRow = seatsInRow;
+	}
+
 	
 
 }
