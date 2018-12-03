9c0f5a6710925c68f4751e9809ede632ec5f58e3
AtanasCh <atanas1337@gmail.com>
2018-12-03 13:42:17 +0200

    Test. Added new functions

--- cinema-soft/src/main/java/softServe/academy/cinemasoft/controller/ScreeningController.java
+++ cinema-soft/src/main/java/softServe/academy/cinemasoft/controller/ScreeningController.java
@@ -1,9 +1,19 @@
 package softServe.academy.cinemasoft.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import softServe.academy.cinemasoft.model.Screening;
 import softServe.academy.cinemasoft.service.ScreeningService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+import static com.sun.deploy.trace.Trace.flush;
+import static org.springframework.transaction.support.TransactionSynchronizationManager.clear;
 
 @Controller
 public class ScreeningController {
@@ -19,6 +29,46 @@ public class ScreeningController {
 
     // DELETE SCREENING
 
-    // ADD SCREENING
+//    @GetMapping("/screening/{id}")
+//    public ResponseEntity<?> getScreeningById(@PathVariable int id){
+//        Screening result = this.screeningService.getScreeningById(id);
+//        if (result != null){
+//            return ResponseEntity.status(HttpStatus.CREATED).build();
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+
+    @GetMapping("/screening")
+    public ResponseEntity<List<Screening>> getAllScreenings(){
+        return ResponseEntity.ok(this.screeningService.findAllScreenings());
+    }
+
+//    @RequestMapping("/remove/{id}")
+//    public ResponseEntity<?> deleteScreeningById(@PathVariable int id) {
+//        Screening screening = new Screening();
+//        screening.setId(id);
+//        entityManager.persist(screening);
+//        flushAndClear();
+//        screening = entityManager.find(Screening.class, screening.getId());
+//        assertThat(screening, notNullValue());
+//        entityManager.remove(foo);
+//        flushAndClear();
+//        assertThat(entityManager.find(Screening.class, screening.getId()), nullValue());
+//    }
+
+//    @RequestMapping(value ="/screening/remove/{id}")
+//    public String deleteScreening(@PathVariable("id") int Id){
+//        screeningService.delete(id);
+//        return "redirect:/screening";
+//    }
+//
+//    // ADD SCREENING
+//
+//    private void flushAndClear() {
+//        flush();
+//        clear();
+//    }
+
 
 }
--- cinema-soft/src/main/java/softServe/academy/cinemasoft/service/ScreeningService.java
+++ cinema-soft/src/main/java/softServe/academy/cinemasoft/service/ScreeningService.java
@@ -5,6 +5,7 @@ import softServe.academy.cinemasoft.model.Screening;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 
 @Service
@@ -13,8 +14,30 @@ public class ScreeningService {
     private ScreeningRepository screeningRepository;
 
     @Autowired
+    public ScreeningService(ScreeningRepository screeningRepository){
+        this.screeningRepository = screeningRepository;
+    }
+
+    public List<Screening> findAllScreenings(){
+        return this.screeningRepository.findAll();
+    }
+
+
     public Screening addScreening(Screening screening){
     return this.screeningRepository.save(screening);
     }
+
+    void deleteScreening(Screening screening){
+        this.screeningRepository.delete(screening);
+    }
+
+//    public Screening getScreeningById(int id) {
+//        return ScreeningRepository.getOne(id);
+//    }
+
+    // EDIT SCREENING
+
+
+
 
 }
