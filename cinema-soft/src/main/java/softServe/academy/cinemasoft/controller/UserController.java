b3feb35a2200efb4bb8298e9157a47cb8b3e0791
tthebestinworld <spambg97@gmail.com>
2018-12-09 11:43:22 +0200

    Fixing register and login links.

--- cinema-soft/src/main/java/softServe/academy/cinemasoft/controller/UserController.java
+++ cinema-soft/src/main/java/softServe/academy/cinemasoft/controller/UserController.java
@@ -29,17 +29,6 @@ public class UserController {
         return "login";
     }
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
-
-    @GetMapping("/test")
-    public String test() {
-        return "test";
-    }
-
 
     @GetMapping("/register")
     public ModelAndView getRegisterForm(Model model){
