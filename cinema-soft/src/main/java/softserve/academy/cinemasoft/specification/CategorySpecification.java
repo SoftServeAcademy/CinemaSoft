13cded42b1584786697cc9e26167867835aa4b4b
jdavi <spambg97@gmail.com>
2018-12-21 12:36:14 +0200

    Refactoring

--- cinema-soft/src/main/java/softserve/academy/cinemasoft/specification/CategorySpecification.java
+++ cinema-soft/src/main/java/softserve/academy/cinemasoft/specification/CategorySpecification.java
@@ -1,4 +1,4 @@
-package softserve.academy.cinemasoft.dto;
+package softserve.academy.cinemasoft.specification;
 
 import org.springframework.data.jpa.domain.Specification;
 import softserve.academy.cinemasoft.model.Category;
