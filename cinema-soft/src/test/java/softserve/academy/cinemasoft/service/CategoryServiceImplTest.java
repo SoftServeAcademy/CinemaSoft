fea42f4e3b03a20004dc22c9b6c3a90793a7c5c3
Yana Dimitrova <ydimi@softserveinc.com>
2018-12-27 15:31:23 +0200

    pom edit, CategoryServiceTestImpl edit

--- cinema-soft/src/test/java/softserve/academy/cinemasoft/service/CategoryServiceImplTest.java
+++ cinema-soft/src/test/java/softserve/academy/cinemasoft/service/CategoryServiceImplTest.java
@@ -1,5 +1,6 @@
 package softserve.academy.cinemasoft.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,33 +10,77 @@ import org.springframework.beans.factory.annotation.Autowired;
 import softserve.academy.cinemasoft.controller.CategoryController;
 import softserve.academy.cinemasoft.model.Category;
 import softserve.academy.cinemasoft.repository.CategoryRepository;
+import softserve.academy.cinemasoft.repository.MovieRepository;
+
+import java.util.Optional;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.only;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 
 public class CategoryServiceImplTest {
 
+
     @Mock
     private CategoryRepository categoryRepository;
 
-    @InjectMocks
-    private CategoryController categoryController;
+    @Mock
+    private MovieRepository movieRepository;
 
-    @Autowired
-    private Category categoryToTest;
-
-    @InjectMocks
     private CategoryServiceImpl categoryService;
 
-    @Test
-    public void testAddCategory(){
-        //categoryToTest.setId(1);
-        categoryService.addCategory(categoryToTest);
-        verify(categoryRepository, only()).save(categoryToTest);
-
+    @Before
+    public void setUp() {
+        categoryService = new CategoryServiceImpl(categoryRepository);
     }
+
+    @Test
+    public void testAddCategory() {
+        Category category = new Category();
+        when(categoryRepository.save(category)).thenReturn(category);
+
+        Category result = categoryService.addCategory(category);
+
+        verify(categoryRepository, only()).save(category);
+        assertEquals(category, result);
+    }
+
+    @Test
+    public void testRemoveCategory() {
+        Category category = new Category();
+        categoryService.removeCategory(anyInt());
+
+        verify(categoryRepository, only()).deleteById(anyInt());
+    }
+
+    @Test
+    public void testEditCategory() {
+        Category category = new Category();
+        when(categoryRepository.save(category)).thenReturn(category);
+        when(categoryRepository.getOne(anyInt())).thenReturn(category);
+
+        categoryService.editCategory(5, "");
+
+        verify(categoryRepository, times(1)).save(category);
+        verify(categoryRepository, times(1)).getOne(5);
+    }
+
+    @Test
+    public void testFindCategoryById() {
+        Category category = new Category();
+        when(categoryRepository.findById(anyInt())).thenReturn(Optional.of(category));
+
+        categoryService.findCategoryById(5);
+
+        verify(categoryRepository, only()).findById(5);
+    }
+
+    @Test
+    public void testFindAll() {
+        categoryService.findAll();
+        verify(categoryRepository, only()).findAll();
+    }
+
 
 }
