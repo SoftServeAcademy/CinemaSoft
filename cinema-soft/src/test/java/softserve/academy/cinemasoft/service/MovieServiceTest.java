afe36e82a06454aa79bfeddd6c5b8760a1d9955d
Yana Dimitrova <ydimi@softserveinc.com>
2019-01-30 15:30:48 +0200

    checkpoint

--- cinema-soft/src/test/java/softserve/academy/cinemasoft/service/MovieServiceTest.java
+++ cinema-soft/src/test/java/softserve/academy/cinemasoft/service/MovieServiceTest.java
@@ -1,24 +1,24 @@
-package softserve.academy.cinemasoft.service;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import softserve.academy.cinemasoft.model.Movie;
-import softserve.academy.cinemasoft.repository.MovieRepository;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.*;
-
-@RunWith(MockitoJUnitRunner.class)
-public class MovieServiceTest {
-
-
-}
+//package softserve.academy.cinemasoft.service;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import softserve.academy.cinemasoft.model.Movie;
+//import softserve.academy.cinemasoft.repository.MovieRepository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.ArgumentMatchers.anyInt;
+//import static org.mockito.Mockito.*;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class MovieServiceTest {
+//
+//
+//}
